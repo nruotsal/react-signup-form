0398be3861ca5f35960405423f91dc78f8ee16af
Niko Ruotsalainen <niko.ruotsalainen@wowanders.com>
2019-09-28 19:04:55 +0300

    Updates

---
+++ precache-manifest.3b392ed8441174d136413fca509efc87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f79b81decd4f9c400fa54f6cd90b560",
+    "url": "/react-signup-form/index.html"
+  },
+  {
+    "revision": "8626f8ede3e2b24010c5",
+    "url": "/react-signup-form/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8626f8ede3e2b24010c5",
+    "url": "/react-signup-form/static/js/2.afd73fa1.chunk.js"
+  },
+  {
+    "revision": "e25106de6d381b618b2b",
+    "url": "/react-signup-form/static/js/main.4ec0f4d6.chunk.js"
+  },
+  {
+    "revision": "c0a6bc9d701d2f98f5f5",
+    "url": "/react-signup-form/static/js/runtime-main.41d2ff7a.js"
+  },
+  {
+    "revision": "d36bd4a7a15f50d1a42ad58049254a48",
+    "url": "/react-signup-form/static/media/delete.d36bd4a7.svg"
+  },
+  {
+    "revision": "c14e32e354aae9a41862c369e7a596c2",
+    "url": "/react-signup-form/static/media/down.c14e32e3.svg"
+  },
+  {
+    "revision": "fe223b7bb2e2485491b75381e6b16b60",
+    "url": "/react-signup-form/static/media/edit.fe223b7b.svg"
+  }
+]);
