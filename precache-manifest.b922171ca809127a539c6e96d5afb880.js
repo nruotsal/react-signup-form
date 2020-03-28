9e822b302ddce86eee73be5622babf33634bf39a
Niko Ruotsalainen <niko.ruotsalainen@wowanders.com>
2020-03-28 18:47:48 +0200

    Updates

--- precache-manifest.b922171ca809127a539c6e96d5afb880.js
+++ precache-manifest.b922171ca809127a539c6e96d5afb880.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f79b81decd4f9c400fa54f6cd90b560",
+    "revision": "cce4946b5a219d4f09acac40864c6061",
     "url": "/react-signup-form/index.html"
   },
   {
-    "revision": "8626f8ede3e2b24010c5",
+    "revision": "5e1db494ce9173e489a7",
     "url": "/react-signup-form/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "8626f8ede3e2b24010c5",
-    "url": "/react-signup-form/static/js/2.afd73fa1.chunk.js"
+    "revision": "5e1db494ce9173e489a7",
+    "url": "/react-signup-form/static/js/2.4274b13d.chunk.js"
   },
   {
-    "revision": "e25106de6d381b618b2b",
-    "url": "/react-signup-form/static/js/main.4ec0f4d6.chunk.js"
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/react-signup-form/static/js/2.4274b13d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0a6bc9d701d2f98f5f5",
-    "url": "/react-signup-form/static/js/runtime-main.41d2ff7a.js"
+    "revision": "75fceb331c2c19ae4745",
+    "url": "/react-signup-form/static/js/main.ff74fe28.chunk.js"
+  },
+  {
+    "revision": "2cf1067fc3b9fd360aa5",
+    "url": "/react-signup-form/static/js/runtime-main.6b689031.js"
   },
   {
     "revision": "d36bd4a7a15f50d1a42ad58049254a48",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-signup-form/precache-manifest.3b392ed8441174d136413fca509efc87.js"
+  "/react-signup-form/precache-manifest.b922171ca809127a539c6e96d5afb880.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-signup-form/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
